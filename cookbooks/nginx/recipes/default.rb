c9d80217e09996d7dc46b2aab2c1e6fbbea2e122
huitae-rightscale <huitae.kim@rightscale.com>
2012-11-13 09:14:28 -0800

    add stop/remove nginx before install

--- cookbooks/nginx/recipes/default.rb
+++ cookbooks/nginx/recipes/default.rb
@@ -7,6 +7,20 @@
 
 rightscale_marker :begin
 
+service "nginx" do
+  action :stop
+  only_if do
+    File.exists?("/var/run/nginx.pid")
+  end
+end
+
+package "nginx" do
+  action :remove
+  only_if do
+    File.exists?("/etc/init.d/nginx")
+  end
+end
+
 package "nginx" do
   action :install
 end
